2118369b4b226a613e1dac6b4be88e342aec7a82
dariatsvetkova <d.s.tsvetkova@hotmail.com>
2022-05-28 14:10:00 -0400

    remove old project

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import * as styles from "../styles/projectCard.module.scss";
-import clapCard from "../data/images/clapcard/home.gif"
 
 const ProjectCard = (props) => {  
   const { slug, number, title, descr, tags} = props.project
@@ -13,29 +12,11 @@ const ProjectCard = (props) => {
       <small className={styles.numCard}>{number < 10 ? "0" + number : number}</small>
       <h4>{title}</h4>
 
-      {slug === "/clapcard"
-        ? <div 
-            className={styles.projectImg}
-            style={{
-              objectFit: "contain",
-            }}
-          >
-            <img
-              src={clapCard}
-              alt={`${title} project screenshot`}
-              style={{
-                maxWidth: '700px',
-                margin: '0',
-                borderRadius: "9px",
-              }}
-            />
-          </div>
-        : <GatsbyImage
-            className={styles.projectImg}
-            image={getImage(props.imgFiles[0].node)}
-            alt={`${title} project screenshot`}
-          />
-      }
+      <GatsbyImage
+        className={styles.projectImg}
+        image={getImage(props.imgFiles[0].node)}
+        alt={`${title} project screenshot`}
+      />
 
       <p className={styles.descr}>{descr} <span className={`textLink ${styles.moreLink}`}>Learn more</span></p>
       <div className={styles.stackTags}>
