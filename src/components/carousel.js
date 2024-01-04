4e85645c4ff4a3be7f1cb8e29c0c2d8eb5c69a80
dariatsvetkova <d.s.tsvetkova@hotmail.com>
2024-01-04 18:59:41 -0500

    replace carousel with react-based one

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,24 +1,97 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react"
+import { ArrowLeft, ArrowRight } from "../components/icons";
 import CarouselCard from "./carouselCard"
 
 import * as styles from "../styles/carousel.module.scss"
 
-const Carousel = (props) => {
-  const total = props.recos.length
+
+const getButtonText = (currentInd, lastInd) => {
+  const prevText = currentInd === 0 ?
+  `Go to last slide` :
+  `Go to previous slide`
   
+  const nextText = currentInd === lastInd ?
+    `Go to first slide` :
+    `Go to next slide`
+
+  return { prevText, nextText}
+}
+
+
+const Carousel = (props) => {
+  const recos = props.recos
+  const lastInd = recos.length - 1
+
+  const [currentSlide, setCurrentSlide] = useState(Math.floor(lastInd / 2))
+
+  const handleNext = () => {
+    if (currentSlide === lastInd) {
+      setCurrentSlide(0)
+    } else {
+      setCurrentSlide(currentSlide + 1)
+    }
+  }
+
+  const handlePrev = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(lastInd)
+    } else {
+      setCurrentSlide(currentSlide - 1)
+    }
+  }
+
+  // create an array that will hold references to each recommendation child
+  const childrenRefs = recos.map(_ => React.createRef())
+
+  useEffect(() => {
+    const currentRecoRef = childrenRefs[currentSlide]
+    if (currentRecoRef?.current) {
+      currentRecoRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "center"
+      })
+    }
+  }, [currentSlide, childrenRefs])
+
+  const { prevText, nextText } = getButtonText(currentSlide, lastInd)
+
   return (
-    <div className={styles.carouselContainer}>
+    <>
+      <div className={styles.background} />
+
+      <div className={styles.leftGradient} />
+      <button
+        className={styles.carouselPrevBtn}
+        aria-label={prevText}
+        onClick={handlePrev}
+      >
+        <ArrowLeft />
+      </button>
+
       <ul className={styles.carousel}>
+        <CarouselCard empty />
+
         {props.recos.map((reco, ind) => (
           <CarouselCard
-            key={reco.id}
+            key={reco.node.id}
             reco={reco}
-            index={ind}
-            total={total}
+            ref={childrenRefs[ind]}
           />
         ))}
+
+        <CarouselCard empty />
       </ul>
-    </div>
+
+      <div className={styles.rightGradient} />
+      <button
+        className={styles.carouselNextBtn}
+        aria-label={nextText}
+        onClick={handleNext}
+      >
+        <ArrowRight />
+      </button>
+    </>
   )
 }
 
--- src/components/carouselCard.js
+++ src/components/carouselCard.js
@@ -1,59 +1,26 @@
 import * as React from "react"
-import { ArrowLeft, ArrowRight } from "../components/icons";
 
 import * as styles from "../styles/carouselCard.module.scss"
 
-const CarouselCard = (props) => {
-  const { from: { name, relation, company }, text } = props.reco.node
-  const { index, total } = props
+const CarouselCard = React.forwardRef((props, ref) => {
+  if (props.empty) {
+    return (
+      <li className={`${styles.card} ${styles.empty}`} aria-hidden="true" />
+    )
+  }
 
-  const prevId = index === 0 ?
-    `#reco-carousel-${total - 1}` :
-    `#reco-carousel-${index - 1}`
-  const prevText = index === 0 ?
-    `Go to last slide` :
-    `Go to previous slide`
-  
-  const nextId = index === total - 1 ?
-    `#reco-carousel-${0}` :
-    `#reco-carousel-${index + 1}`
-  const nextText = index === total - 1 ?
-    `Go to first slide` :
-    `Go to next slide`
-
+  const { from: { name, relation, company }, text } = props.reco?.node
 
   return (
-    <li 
-      id={`reco-carousel-${index}`}
-      tabindex="0"
-      className={styles.card}
-    >
-      <div className={styles.carouselArrows}>
-        <a
-          href={prevId}
-          className="carouselPrev"
-          ariaLabel={prevText}
-        >
-          <ArrowLeft />
-        </a>
-
-        <a
-          href={nextId}
-          className="carouselNext"
-          ariaLabel={nextText}
-        >
-          <ArrowRight />
-        </a>
-      </div>
-
+    <li className={styles.card} ref={ref}>
       <div className={styles.textContainer}>
-        <p className={styles.text}>{text}</p>
+        <p>{text}</p>
         <p className={styles.from}>
           <small>— {name}, {relation} at {company}</small>
         </p>
       </div>
     </li>
   )  
-}
+})
 
 export default CarouselCard
